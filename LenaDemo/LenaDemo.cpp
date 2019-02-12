b7ee2fb9d0b1d038f5cbe3509c6322ebd26fde6a
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-12 15:19:51 -0500

    added hough line transform

--- LenaDemo/LenaDemo.cpp
+++ LenaDemo/LenaDemo.cpp
@@ -9,6 +9,7 @@
 using namespace cv;
 using namespace std;
 
+
 int main()
 {
 	VideoCapture cap(0); // open the default camera
@@ -32,12 +33,38 @@ int main()
 			cout << scale << endl;
 		}
 
-		Mat frame;
+		Mat frame, cdst;
 		cap >> frame; // get a new frame from camera
-		cvtColor(frame, edges, COLOR_BGR2GRAY);
-		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, scale);
-		imshow("edges", edges);
+		Canny(frame, edges, 50, 300, scale);
+		cvtColor(edges, cdst, COLOR_GRAY2BGR);
+		
+		
+		#if 0
+		vector<Vec2f> lines;
+		HoughLines(edges, lines, 1, CV_PI / 180, 100, 0, 0);
+
+		for (size_t i = 0; i < lines.size(); i++)
+		{
+			float rho = lines[i][0], theta = lines[i][1];
+			Point pt1, pt2;
+			double a = cos(theta), b = sin(theta);
+			double x0 = a * rho, y0 = b * rho;
+			pt1.x = cvRound(x0 + 1000 * (-b));
+			pt1.y = cvRound(y0 + 1000 * (a));
+			pt2.x = cvRound(x0 - 1000 * (-b));
+			pt2.y = cvRound(y0 - 1000 * (a));
+			line(cdst, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+		}
+		#else
+		vector<Vec4i> lines;
+		HoughLinesP(edges, lines, 1, CV_PI / 180, 50, 50, 10);
+		for (size_t i = 0; i < lines.size(); i++)
+		{
+			Vec4i l = lines[i];
+			line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+		}
+		#endif
+		imshow("edges", cdst);
 		if ((waitKey(30) >= 0) && !(GetKeyState('A') & 0x8000/*Check if high-order bit is set (1 << 15)*/) && !(GetKeyState('Z') & 0x8000)) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
